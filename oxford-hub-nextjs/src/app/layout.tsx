d5aaaea60f6bf963baa737278e4c0b4140b7672b
ohsglobal <digital.admin@ohs.global>
2025-10-09 00:02:15 -0400

    feat: add Google Tag Manager tracking
    - Add GTM script to head section (GTM-KHPLVQWC)
    - Add GTM noscript fallback to body section
    - Enable analytics tracking for website performance monitoring

--- oxford-hub-nextjs/src/app/layout.tsx
+++ oxford-hub-nextjs/src/app/layout.tsx
@@ -16,8 +16,25 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://use.typekit.net/gcy4fhs.css" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KHPLVQWC');`
+          }}
+        />
       </head>
       <body>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KHPLVQWC"
+            height="0" 
+            width="0" 
+            style={{display: 'none', visibility: 'hidden'}}
+          />
+        </noscript>
         {children}
         <Script
           src="https://consent.trustarc.com/notice?domain=omers-opgi-corporate.com&c=teconsent&js=nj&noticeType=bb&pcookie&gtm=1&text=true"
